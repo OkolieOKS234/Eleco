cdfca8716d88398ca9955d5636403a3aeccae6a7
OkolieOKS234 <dokolie040@gmail.com>
2024-10-01 18:17:01 +0100

    added a bit on Mongoose ID validation

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,8 +1,23 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
 export default (err, req, res, next) => {
     let error ={
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal Server Error",
     };
+
+    // Handle Mongoose ID error
+if(err.name === 'CastError'){
+    const message = `Resource not found, Invalid: ${err?.path}`
+    error = new ErrorHandler(message, 404);
+}
+// Handle Validation Error
+if(err.name === 'ValidatorError'){
+    const message = Object.values(err.errors)
+    error = new ErrorHandler(message, 404);
+}
+
+
 
 if(process.env.NODE_ENV === "DEVELOPMENT"){
     res.status(error.statusCode).json({
@@ -17,5 +32,5 @@ if(process.env.NODE_ENV === "PRODUCTION"){
         message: error.message,
     })  
 }
-
+  
 }
